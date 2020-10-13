6e33c9ddae5b3275193d0ded2a5fee71a333f3b4
JuanchoPancho <torrablofam17@gmail.com>
2020-10-13 20:40:05 +0200

    changes made

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const os = require('os');
+const fs = require('fs');
 
-console.log(os.platform());
-console.log(os.release());
-console.log('free mem:',os.freemem(),'Bytes');
+fs.writeFile('./text.txt','linea 1',function (err) {
+    if (err){
+        console.log(err);
+    }
+    console.log('File created')
+})
